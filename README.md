9ec789df635c68f046c87519fa15eda3ca147b0e
jamesshep64AM <jamesshep@tamu.edu>
2024-02-20 14:52:16 -0600

    Read me update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DataGenerator
+Data generator to generate data for the SQL data base. Using python, saving data into a .csv file.
---
+++ datagen.py
@@ -0,0 +1,72 @@
+import random
+f = open("data.csv","w")
+
+burgersMenu = {"Rev's Burger":5.59,
+           "Double Stach Cheese Burger":8.79,
+           "Classic Burger": 5.49,
+           "Bacon Cheeseburger":6.99}
+
+burgersComboPrice = 1.90
+
+baskets = {"Three Tender Basket":6.79,
+           "Four Steak Finger Basket":8.39}
+
+basketsComboPrice = 1.10
+
+sandwichesMenu = {"Gig 'em Patty Melt": 6.29,
+              "Howdy Spicy Ranch Chicken Strip":6.99,
+              "Classic Crispy Chicket Tender Sandwich":5.79,
+              "Grilled Chicken Tender Sandwich":5.79,
+              "Grilled Cheese":3.49}
+
+gigPattyMeltComboPrice = 1.80
+sandwichesComboPrice = 1.90
+
+shakesNSweetMenu = {"Aggie Shake":3.14,
+                "Double Scoop Ice Cream Cup":3.79,
+                "Chocolate Chip Chunk Cookie":2.11,
+                "Chocolate Fudge Brownie":2.11}
+
+saucesMenu = {"Gig 'Em Sauce":.69,
+          "Buffalo":.69,
+          "Ranch":.69,
+          "BBQ Sauce":.69,
+          "Honey Mustard":.69,
+          "Spicy Ranch":.69}
+
+sidesMenu = {"Seasoned Fries":1.5,
+         "Tator Tots":1.5,
+         "Onion Rings":1.98,
+         "Kettle Chips":1.25}
+
+beveragesMenu = {"Medium Fountain Drink":2.25,
+             "Large Fountain Drink":2.45,
+             "Drip Coffee":2.29,
+             "Cold Brew":3.65}
+
+customerNames = [
+    "Liam",
+    "Olivia",
+    "Noah",
+    "Emma",
+    "Oliver",
+    "Ava",
+    "Elijah",
+    "Charlotte",
+    "William",
+    "Sophia",
+    "James",
+    "Amelia",
+    "Benjamin",
+    "Isabella",
+    "Lucas",
+    "Mia",
+    "Henry",
+    "Evelyn",
+    "Alexander",
+    "Harper"
+]
+
+managers = ["Zach","Xavier","Victor"]
+
+employees = ["Quency","Wyat","Emily","Ryan","Trist","Yorn","Uno"]
